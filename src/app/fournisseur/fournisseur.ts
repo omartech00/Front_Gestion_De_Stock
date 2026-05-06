20d1fac9ccabef70fccdbe3f094b781c27e7c707
Omar <elhadjiomardieng5@gmail.com>
2026-05-06 23:46:23 +0000

    Page de commande client1

--- src/app/fournisseur/fournisseur.ts
+++ src/app/fournisseur/fournisseur.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './fournisseur.html',
   styleUrl: './fournisseur.css',
 })
-export class Fournisseur {}
+export class Fournisseur {
+  fournisseurs: any[] = [
+    {nom: 'Fournisseur 1', email: 'fournisseur1@example.com', tel: '0123456789', action: 'Commander'},
+    {nom: 'Fournisseur 2', email: 'fournisseur2@example.com', tel: '0123456789', action: 'Commander'},
+    {nom: 'Fournisseur 3', email: 'fournisseur3@example.com', tel: '0123456789', action: 'Commander'},
+    {nom: 'Fournisseur 4', email: 'fournisseur4@example.com', tel: '0123456789', action: 'Commander'},
+    {nom: 'Fournisseur 5', email: 'fournisseur5@example.com', tel: '0123456789', action: 'Commander'}
+  ];
+}
--- src/app/new-order/new-order.ts
+++ src/app/new-order/new-order.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './new-order.html',
   styleUrl: './new-order.css',
 })
-export class NewOrder {}
+export class NewOrder {
+  price:number = 8000;
+  produits: any[] = [{id: 1, name: 'Poutre Acier', description: 'H-Section 400mm', price: 100},
+                    {id: 2, name: 'Aluminium', description: 'H-Section 300mm', price: 150},
+                    {id: 3, name: 'Poutre Fer', description: 'H-Section 500mm', price: 200},
+                    {id: 4, name: 'Poutre PVC', description: 'H-Section 200mm', price: 50},
+                    {id: 5, name: 'Poutre Bois', description: 'H-Section 600mm', price: 120},
+                    {id: 6, name: 'Poutre Bois', description: 'H-Section 600mm', price: 180},
+                    {id: 7, name: 'Poutre Bois', description: 'H-Section 600mm', price: 240},
+                    {id: 8, name: 'Poutre Bois', description: 'H-Section 600mm', price: 300},
+                    {id: 9, name: 'Poutre Bois', description: 'H-Section 600mm', price: 360},
+                    {id: 10, name: 'Poutre Bois', description: 'H-Section 600mm', price: 420}
+
+                    ];
+}
