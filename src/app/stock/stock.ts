97cd75ba9ca3436883c1aadbff71fef23367ee95
Omar <elhadjiomardieng5@gmail.com>
2026-05-06 13:40:10 +0000

    Implement dashboard-stock-vente interfaces

--- src/app/stock/stock.ts
+++ src/app/stock/stock.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './stock.html',
   styleUrl: './stock.css',
 })
-export class Stock {}
+export class Stock {
+  produits: any[] = [{id: 1, name: 'Produit 1', categorie: 'Catégorie 1', stock: 100, status: 'En stock' },
+                    {id: 2, name: 'Produit 2', categorie: 'Catégorie 2', stock: 5, status: 'Stock bas' },
+                    {id: 3, name: 'Produit 3', categorie: 'Catégorie 3', stock: 0, status: 'En rupture' },
+                    {id: 4, name: 'Produit 4', categorie: 'Catégorie 4', stock: 150, status: 'En stock' },
+                    {id: 5, name: 'Produit 5', categorie: 'Catégorie 5', stock: 8, status: 'Stock bas' },
+                    {id: 6, name: 'Produit 6', categorie: 'Catégorie 6', stock: 25, status: 'En stock' },
+                    {id: 7, name: 'Produit 7', categorie: 'Catégorie 7', stock: 0, status: 'En rupture' },
+                    {id: 8, name: 'Produit 8', categorie: 'Catégorie 8', stock: 10, status: 'Stock bas' },
+                    {id: 9, name: 'Produit 9', categorie: 'Catégorie 9', stock: 50, status: 'En stock' },
+                    
+                    ];
+}
--- src/app/ventes/ventes.ts
+++ src/app/ventes/ventes.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './ventes.html',
   styleUrl: './ventes.css',
 })
-export class Ventes {}
+export class Ventes {
+  ventes: any[] = [{id: 9900, date: '2026-01-01 15h30min05s', nbr_article: '8', facture: 10000, vendeur: 'Moussa', status: 'Validé' },
+                  {id: 9901, date: '2026-01-02 10h15min20s', nbr_article: '5', facture: 7500, vendeur: 'Amina', status: 'Validé'},
+                  {id: 9902, date: '2026-01-03 14h45min30s', nbr_article: '12', facture: 15000, vendeur: 'Karim', status: 'Annulé'  },
+                  {id: 9903, date: '2026-01-04 09h20min10s', nbr_article: '3', facture: 4500, vendeur: 'Moussa', status: 'Validé' },
+                  {id: 9904, date: '2026-01-05 16h50min40s', nbr_article: '7', facture: 10500, vendeur: 'Amina', status: 'Validé' },
+                  {id: 9905, date: '2026-01-06 11h10min50s', nbr_article: '10', facture: 12500, vendeur: 'Amina', status: 'Validé' },
+                  {id: 9906, date: '2026-01-07 13h25min15s', nbr_article: '4', facture: 6000, vendeur: 'Moussa', status: 'Validé' },
+                  {id: 9907, date: '2026-01-08 12h00min00s', nbr_article: '6', facture: 9000, vendeur: 'Amina', status: 'Validé' }
+                    ];
+}
